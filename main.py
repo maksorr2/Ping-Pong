b70c3180dc012aba4f9b926e7cdbfff954594ef8
maksorr2 <vanya.markov.75@bk.ru>
2023-04-17 21:41:33 +0300

    beta v.3
    Сделано движение мячика, хоть и корявое

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ display.set_caption('Шутер')
 clock = time.Clock()
 background = transform.scale(image.load('fon.jpg'), (700, 500))
 
+circle_top = None
+circle_right = None
+
 win_height = 500
 win_width = 700
 
@@ -36,10 +39,30 @@ class Player2(GameSprite):
         if key_pressed[K_RIGHT] and self.rect.y < win_height - 80:
             self.rect.y += self.speed
 class Ball(GameSprite):
-    pass
+    def update(self):
+        self.rect.x += self.speed
+        if self.rect.x >= 650:
+            global circle_right
+            circle_right = True
+        if self.rect.y <= 5:
+            global circle_top
+            circle_top = True
+        else:
+            circle_top = False
+        if self.rect.y > 450:
+            self.rect.y -= self.speed
+        if circle_right:
+            self.rect.x -= self.speed
+        else: 
+            self.rect.y -= self.speed
+        if circle_top:
+            self.rect.y -= self.speed
+        else: 
+            self.rect.y += self.speed
 
 player1 = Player1('player1.png', 5, win_height -80, 4, 80, 1 )
 player2 = Player2('player2.png', 635, win_height -85, 4, 80, 1 )
+ball = Ball('ball.png', 100, 100, 10, 10, 1)
 game = True
 finish = False
 FPS = 60
@@ -53,4 +76,6 @@ while game:
         player1.update()
         player2.reset()
         player2.update()
+        ball.reset()
+        ball.update()
         display.update()
